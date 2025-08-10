66e8ee62ab5890ba190e14e4ffc27eb3f53a5f0f
Ruslan Starikov <ruslan@beef-burger.com>
2025-08-10 17:58:33 +1000

    LOL

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\RoundController;
 use App\Http\Controllers\GameController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/game');
 });
 Route::get('/game', [GameController::class, 'index'])->name('game.index');
 Route::post('/game/round', [GameController::class, 'round'])->name('game.round');
